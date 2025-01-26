55d4e737919b01867ac55b47ff15aa0a9feb627e
jacquarde <jacquarde.appeels@gmail.com>
2025-01-26 21:07:48 +0100

    🔨 Add testing layout and framework
    Kotest framewor dependencies.
    Testing porject layout.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,51 @@
+/*
+ * Copyright 2025 jacquarde
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 plugins {
 	alias(main.plugins.kotlin)
 }
 
-group = "io.github.jacquarde"
-version = "0.1"
 
-tasks.wrapper {
-	distributionType	= Wrapper.DistributionType.ALL
-	gradleVersion		= main.versions.gradle.get()
-}
+group	= "io.github.jacquarde"
+version	= "0.1"
+
 
 kotlin {
 	jvmToolchain {
 		languageVersion	= JavaLanguageVersion.of(main.versions.jvm.get())
 		vendor			= JvmVendorSpec.GRAAL_VM
 	}
+	sourceSets {
+		test {
+			kotlin.srcDirs("src/test.functional/kotlin")
+			resources.srcDirs("src/test.functional/resources")
+		}
+	}
+	dependencies {
+		testImplementation(libs.kotest)
+	}
 }
 
-dependencies {}
-
-tasks.test {
-	useJUnitPlatform()
+tasks {
+	test {
+		useJUnitPlatform()
+	}
+	wrapper {
+		distributionType	= Wrapper.DistributionType.ALL
+		gradleVersion		= main.versions.gradle.get()
+	}
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,27 @@
-rootProject.name = "gradle-build-summary"
+/*
+ * Copyright 2025 jacquarde
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 
 plugins {
 	id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
+
+
+rootProject.name = "gradle-build-summary"
+
 
 dependencyResolutionManagement {
 	repositories {
@@ -13,6 +32,10 @@ dependencyResolutionManagement {
 			version("gradle", "8.12.1")
 			version("jvm", "23")
 			plugin("kotlin","org.jetbrains.kotlin.jvm").version("2.1.0")
+		}
+		create("libs") {
+			version("kotest", "5.9.1")
+			library("kotest", "io.kotest", "kotest-runner-junit5").versionRef("kotest")
 		}
 	}
 }
