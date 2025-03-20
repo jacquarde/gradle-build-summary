9a512e8e302b215fd0c7aa4d2cce86f9f0e4e27c
jacquarde <jacquarde@proton.me>
2025-03-20 05:48:07 +0100

    💚

--- .github/scripts/capture-properties.ps1
+++ .github/scripts/capture-properties.ps1
@@ -4,4 +4,4 @@ $command=".\gradlew properties -q --console=plain"
 $pattern="^${Property}: (.+)$"
 $result=Invoke-Expression ".\gradlew properties -q --console=plain" | Select-String -Pattern $pattern
 $value=$result.Matches[0].Groups[1]
-Write-Host "$Property=$value"
+Write-Output "$Property=$value"
