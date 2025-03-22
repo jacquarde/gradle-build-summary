f4052a3207a9a8dba65f7214faa9f923dd7f83af
jacquarde <jacquarde@proton.me>
2025-03-22 07:42:08 +0100

    🔥 Local GitHub action

--- .github/actions/gradle-task/capture-properties.ps1
+++
@@ -1,7 +0,0 @@
-param ($Property)
-
-$command=".\gradlew properties -q --console=plain"
-$pattern="^${Property}: (.+)$"
-$result=Invoke-Expression ".\gradlew properties -q --console=plain" | Select-String -Pattern $pattern
-$value=$result.Matches[0].Groups[1]
-Write-Output "$Property=$value"
